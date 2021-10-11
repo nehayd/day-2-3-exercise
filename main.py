e9e865fff8917bebe1856361c0bb13a13fabbddd
nehayd <89801073+nehayd@users.noreply.github.com>
2021-10-11 12:52:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# 🚨 Don't change the code below 👇
+age = int(input("What is your current age?"))
+# 🚨 Don't change the code above 👆
+
+#Write your code below this line 👇
+print("You have %s days, %s weeks, and %s months left."%(365*(90-age),52*(90-age),12*(90-age)))
+
+
+
+
+
+
+
+
